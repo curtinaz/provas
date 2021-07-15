d687ca025e2c602d5d358a09e5b5cd244d3d47d1
Cristian Curtinaz <63929112+curtinaz@users.noreply.github.com>
2021-07-15 11:39:32 -0300

    major css changes

--- logado/perfil.php
+++ logado/perfil.php
@@ -1,32 +1,68 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
-    <link rel="stylesheet" href="perfil.css">
-    <title>Perfil</title>
-</head>
-<body>
-    <main>
-        <div class="container">
-            <div class="profile">
-                <i class="far fa-user-circle"></i>
-            </div>
-            <div class="profile-info">
-                <h5>Nome: Developer</h5>
-                <h5>Email: teste@dev</h5>
-                <h5>Idade: 24</h5>
-                <h5>Sexo: masculino</h5>
-                <h5>Estado: RS</h5>
-                <h5>Cidade: Porto Alegre</h5>
-            </div>
-            <div class="profile-info2">
-                
-            </div>
+<?php
 
-        </div>  
-    </main>
-</body>
-</html>
+require_once '../config.php';
+session_start();
+
+if (!isset($_SESSION['logado'])) {
+    echo "Você precisa estar logado para acessar esta página.<br>";
+    echo "<a href='../index.php'>Faça Login</a>";
+    // testa se o usuário está logado
+} else {
+    $name = $_SESSION['name'];
+    $mail = $_SESSION['email'];
+    $userId = $_SESSION['user_id'];
+
+    // $userQuery = $conexao->query("SELECT `nome`, `email` FROM users WHERE email = '$mail' AND senha = '$senha'");
+
+    $pendentSQL = $conexao->query("SELECT * FROM tests WHERE `user_id` = '$userId' AND `made` = '0'");
+    $pendentTests = $pendentSQL->fetch_all(MYSQLI_ASSOC);
+
+    $madeSQL = $conexao->query("SELECT * FROM tests WHERE `user_id` = '$userId' AND `made` = '1'");
+    $madeTests = $pendentSQL->fetch_all(MYSQLI_ASSOC);
+
+    // fecha o php para o html entrar 
+?>
+
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+        <link rel="stylesheet" href="perfil.css">
+        <title>Perfil</title>
+    </head>
+
+    <body>
+        <main>
+            <div class="container">
+                <div class="container-profile">
+                    <div class="back-btn">
+                        <a href="index.php"><i class="fas fa-arrow-left"></i></a>
+                    </div>
+                    <div class="profile">
+                        <i class="far fa-user-circle"></i>
+                    </div>
+                    <div class="profile-info">
+                        <h5>Nome: <?php echo $name; ?></h5>
+                        <h5>Email: <?php echo $mail; ?> </h5>
+                        <h5>Idade: 24</h5>
+                        <h5>Sexo: masculino</h5>
+                        <h5>Estado: RS</h5>
+                        <h5>Cidade: Porto Alegre</h5>
+                    </div>
+                </div>
+                <div class="profile-info2">
+
+                </div>
+
+            </div>
+        </main>
+    </body>
+
+    </html>
+
+<?php } //reinicia o php para poder fechar
+?>
